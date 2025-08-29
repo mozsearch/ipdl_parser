854498454b2b5c5787e65b546762200ba39d357c
Andrew McCreight <continuation@gmail.com>
2025-08-29 10:04:07 -0700

    Change the edition from the default 2015 to 2018.
    2018 matches what most of SearchFox uses.
    
    This only required adding "crate::" to a bunch of use statements.

--- src/type_check.rs
+++ src/type_check.rs
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use ast::*;
-use errors::Errors;
+use crate::ast::*;
+use crate::errors::Errors;
 use std::collections::{HashMap, HashSet};
 
 // C types
